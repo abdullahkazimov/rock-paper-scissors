c392e082fac3eca82b63d1fc814ea931dc7f98b1
abdullahkazimov <abdullahkazimov@outlook.com>
2024-10-06 10:45:43 +0400

    commands set

--- main.py
+++ main.py
@@ -6,12 +6,23 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 bot = telebot.TeleBot(BOT_TOKEN)
 
 commands = [
-    BotCommand(command="start", description="Start Telegram Bot")
+    BotCommand(command="restart", description="Restart the current game"),
+    BotCommand(command="statistics", description="See statistics"),
+    BotCommand(command="about", description="About the bot")
 ]
 
 @bot.message_handler(commands=['start'])
 def send_welcome_message(message):
     bot.reply_to(message, "Hi!")
+
+@bot.message_handler(commands=['statistics'])
+def send_welcome_message(message):
+    bot.reply_to(message, "Hi!")
+
+@bot.message_handler(commands=['about'])
+def send_welcome_message(message):
+    bot.reply_to(message, "Hi!")
+
 
 bot.set_my_commands(commands)
 bot.infinity_polling()
